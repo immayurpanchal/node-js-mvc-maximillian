206f0f9766d4a97f85dc66cf0a396746aba9b768
immayurpanchal <mayurpanchal002@gmail.com>
2019-03-04 10:59:16 +0530

    Added routes

--- app.js
+++ app.js
@@ -2,20 +2,22 @@
 //1. Import http built in node js module
 const http = require("http");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => { 
-    console.log("In the middleware");
-    next(); //It won't go to next middleware if next() is not written
+app.use("/add-product", (req, res, next) => {
+  console.log("In the middleware");
+  next(); //It won't go to next middleware if next() is not written
 });
 
-app.use((req, res, next) => { 
-    console.log("In another middleware");
-    res.send('<html><head><title>Node Practice</title></head><body><h1>Hello from Express.JS</h1></body></html>');
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+  res.send(
+    "<html><head><title>Node Practice</title></head><body><h1>Hello from Express.JS</h1></body></html>"
+  );
 });
-//passing handler function reference to 
+//passing handler function reference to
 const server = http.createServer(app);
 
 // console.log(routes.someText);
