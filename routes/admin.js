b63188a111e025c70594093d77c0ee5a49ebd05a
immayurpanchal <mayurpanchal002@gmail.com>
2019-03-04 11:37:15 +0530

    same path but diff requests

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+//GET request same PATH
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" placeholder="title"/><button type="submit">Submit</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title" placeholder="title"/><button type="submit">Submit</button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+//POST request same PATH
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/"); //Redirect request to defined path
 });
