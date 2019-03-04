a0b7b99ee17974ad1ee8f3c93a4943fa670142cb
immayurpanchal <mayurpanchal002@gmail.com>
2019-03-04 10:57:13 +0530

    Added response in the middleware, package.json

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const app = express();
 
 app.use((req, res, next) => { 
     console.log("In the middleware");
+    next(); //It won't go to next middleware if next() is not written
 });
 
 app.use((req, res, next) => { 
     console.log("In another middleware");
+    res.send('<html><head><title>Node Practice</title></head><body><h1>Hello from Express.JS</h1></body></html>');
 });
 //passing handler function reference to 
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 // console.log(routes.someText);
 
