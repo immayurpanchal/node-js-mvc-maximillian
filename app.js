f9b39eb58e7ed101d8903956686c7ca896416521
immayurpanchal <mayurpanchal002@gmail.com>
2019-03-05 10:29:03 +0530

    Added helper function for path

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ const express = require("express");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const rootDir = require('./util/path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use('/admin', adminRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 const server = http.createServer(app);
--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 //GET request same PATH
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 //POST request same PATH
